504e851083749e083385997c531e3ddffdb47b30
Jose Luis Ramirez Hernandez <jlgamer25.ah@gmail.com>
2022-08-29 19:19:22 -0600

    Matrices dinamicas y MatrixP

--- TareaProcesoHilos.c
+++ TareaProcesoHilos.c
@@ -4,110 +4,108 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-int MatrixT (int tam)
+
+
+
+
+int **generarMatriz (int tam)
 {
-    // falta hacer generacion por punteros o variables globales 
-   int matrix[tam][tam];
+    // generaciòn de una matriz dinamica 
+   int **matriz;
+   matriz = calloc(tam, sizeof(int *));
+    for(int i = 0; i < tam; i++){
+        matriz[i] = (int *) calloc(tam, sizeof(int)); 
+    }
+
+    // asiganaciòn de valores aleatorios
    for(int fil = 0; fil < tam; fil++){
        for(int col=0; col < tam; col++){
-           matrix[fil][col]= rand() % 11;
+           matriz[fil][col]= rand() % 11;
        }
    }
-   return 0;
+   return matriz;
   
 }
-int MatrixP (int tam)
+
+int **multuplicacionMatrices (int **matriz1, int **matriz2,int tam)
 {
-   int matrix[tam][tam];
-   for(int fil = 0; fil < tam; fil++){
-       for(int col=0; col < tam; col++){
-           matrix[fil][col]= rand() % 11;
-       }
-   }
-   //for(int fil = 0; fil < tam; fil++){
-   //   for(int col=0; col < tam; col++){
-   //        printf("%d ", matrix[fil][col]);
-   //    }
-   //     printf("\n");
-   //}
-   return 0;
-}
-int multuplicacionMatrices (int tam)
-{
-    // funciona la multiplicacion falta que reciba las matrices por parametro 
-    int matrix1[tam][tam];
-   for(int fil = 0; fil < tam; fil++){
-       for(int col=0; col < tam; col++){
-           matrix1[fil][col]= rand() % 11;
-       }
-   }
+   int **matriz;
+   matriz = calloc(tam, sizeof(int *));
+    for(int i = 0; i < tam; i++){
+        matriz[i] = (int *) calloc(tam, sizeof(int)); 
+    }
 
-    for(int fil = 0; fil < tam; fil++){
-      for(int col=0; col < tam; col++){
-           printf("%d ", matrix1[fil][col]);
-       }
-        printf("\n");
-   }
-    printf("\n");
-
-     int matrix2[tam][tam];
-   for(int fil = 0; fil < tam; fil++){
-       for(int col=0; col < tam; col++){
-           matrix2[fil][col]= rand() % 11;
-       }
-   }
-
-    for(int fil = 0; fil < tam; fil++){
-      for(int col=0; col < tam; col++){
-           printf("%d ", matrix2[fil][col]);
-       }
-        printf("\n");
-   }
-    printf("\n");
-
-   int matrizRes[tam][tam];
     for (int i = 0; i < tam; i++) {//se itera através de cada fila de matriz1
             for (int j = 0; j < tam; j++) {//se itera através de cada columna de matriz2
                 int suma = 0;//es donde se almacenará el valor final 
                 for (int k = 0; k < tam; k++) {
-                    suma += matrix1[i][k] * matrix2[k][j];//se acumula en suma
+                    suma += matriz1[i][k] * matriz2[k][j];//se acumula en suma
                 }
-                matrizRes[i][j] = suma;
+                matriz[i][j] = suma;
             }
         }
 
+   return matriz;
+}
 
-   for(int fil = 0; fil < tam; fil++){
+void imprimirMatriz(int** matriz, int tam){
+ for(int fil = 0; fil < tam; fil++){
       for(int col=0; col < tam; col++){
-           printf("%d ",matrizRes[fil][col]);
+           printf("%d ",matriz[fil][col]);
        }
         printf("\n");
    }
-   return 0;
+   printf("\n");
 }
+
+void liberarMemoria (int** matriz, int tam){
+ for(int i = 0; i < tam; i++)
+     free(matriz[i]);
+free(matriz);
+  
+}
+
+int MatrixT (int tam)
+{
+    // implementar el programa de hilos
+   return 0; 
+}
+
+
+
+double MatrixP (int tam)
+{
+   double tiempo= 0.0;
+   int **matriz1, **matriz2;
+   matriz1 = generarMatriz(tam);
+   matriz2 = generarMatriz(tam);
+   //clock_t inicio = clock();
+   time_t inicio =  time(NULL);
+   fork();
+   multuplicacionMatrices(matriz1,matriz2, tam);
+   //clock_t fin= clock();
+   time_t fin =  time(NULL);
+   //tiempo += (double)(fin - inicio) / CLOCKS_PER_SEC;
+   tiempo = fin - inicio;
+   liberarMemoria(matriz1,tam);
+   liberarMemoria(matriz2,tam);
+   return tiempo;
+}
+
+double matrizPciclo(int tam){
+   double tiempoMatrixP, tiempoRes;
+   for(int i = 0; i < 100; i++){
+      tiempoMatrixP +=  MatrixP(tam);
+   }
+    tiempoRes =  tiempoMatrixP /100;
+    return  tiempoRes;
+
+}
+
 
  
 int main(void){
-   /*double tiempoMatrixT= 0.0;
-   clock_t inicioMatrixT = clock();
-   for(int i = 0; i < 100; i++){
-       fork (); 
-       MatrixT(10);
-   }
-   clock_t finMatrixT= clock();
-   tiempoMatrixT += (double)(finMatrixT - inicioMatrixT) / CLOCKS_PER_SEC;
-   printf("Tiempo matrixT %f segundos", tiempoMatrixT);
-   printf("\n");
- 
-   double tiempoMatrixP= 0.0;
-   clock_t inicioMatrixP = clock();
-   for(int i = 0; i < 100; i++){
-       MatrixP(10);
-   }
-   clock_t finMatrixP= clock();
-   tiempoMatrixP += (double)(finMatrixP - inicioMatrixP) / CLOCKS_PER_SEC;
-   printf("Timpo matrixP %f segundos", tiempoMatrixP);
-    */
-   multuplicacionMatrices (3);
+   printf("Timpo matrixP %f segundos", matrizPciclo(10));
+
 return 0;
 }
