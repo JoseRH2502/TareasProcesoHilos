298d44ffaaaa4df2efd41f5c2ae7ef64f902cf80
Jose Luis Ramirez Hernandez <jlgamer25.ah@gmail.com>
2022-09-02 22:16:27 -0600

    Trabajo final

--- Procesos/MatrixP.c
+++ Procesos/MatrixP.c
@@ -7,14 +7,8 @@
 #include <unistd.h>
 #include <string.h>
 #include <errno.h>
-
 #define READ_END    0    /* index pipe extremo escritura */
 #define WRITE_END   1    /* index pipe extremo lectura */
-
-int i = 0;
-int j = 0;
-
-
 
 int **generarMatriz (int tam)
 {
@@ -58,22 +52,15 @@ int **multuplicacionMatrices (int **matriz1, int **matriz2, int tam)
 
 
 
-
-
-
-
 int multuplicacionFilxCol (int **matriz1, int **matriz2, int tam, int i, int j){
-    int suma = 0;//es donde se almacenará el valor final 
+    int suma = 0; 
     for (int k = 0; k < tam; k++) {
-        suma += matriz1[i][k] * matriz2[k][j];//se acumula en suma
+        suma += matriz1[i][k] * matriz2[k][j];
     }
 
     return suma;
            
 }
-
-
-
 
 
 void imprimirMatriz(int** matriz, int tam){
@@ -86,12 +73,24 @@ void imprimirMatriz(int** matriz, int tam){
    printf("\n");
 }
 
-int guardarMatrix(int **matriz){
-FILE *fichero = fopen( "Matriz_100.txt", "a" );
-    for( int fila = 0; fila < 3; ++fila ) {
-    for( int col = 0; col < 3; ++col ) 
+int guardarMatrix(int **matriz, int tam){
+FILE *fichero = fopen( "Mat_R.txt", "a" );
+    for( int fila = 0; fila < tam; ++fila ) {
+    for( int col = 0; col < tam; ++col ) 
         fprintf( fichero, "%d ", matriz[fila][col] );
-    fprintf( fichero, "\n" );
+        fprintf( fichero, "\n" );
+  }
+  fprintf( fichero, "\n" );
+  fclose( fichero );
+return 0;
+}
+
+int almacenarMatrix(int **matriz,int tam){
+FILE *fichero = fopen( "Matriz.txt", "w" );
+    for( int fila = 0; fila < tam; ++fila ) {
+    for( int col = 0; col < tam; ++col ) 
+        fprintf( fichero, "%d ", matriz[fila][col] );
+        fprintf( fichero, "\n" );
   }
   fprintf( fichero, "\n" );
   fclose( fichero );
@@ -114,77 +113,69 @@ free(matriz);
   
 }
 
-int MatrixT (int tam)
+int MatrixP (int tam)
 {
-    // implementar el programa de hilos
-   return 0; 
-}
-
-
-
-double MatrixP (int tam)
-{
-   double tiempo= 0.0;
-   int **matriz1, **matriz2, **matriz3;
+   
+   int **matriz1, **matriz2, **matriz;
    matriz1 = generarMatriz(tam);
    matriz2 = generarMatriz(tam);
-   
-   guardarMatrix(matriz1);
-   guardarMatrix(matriz2);
-   time_t inicio =  time(NULL);
-   int fd1[2];
+   guardarMatrix(matriz1,tam);
+   guardarMatrix(matriz2,tam);
+   matriz = calloc(tam, sizeof(int *));
+    for(int i = 0; i < tam; i++){
+        matriz[i] = (int *) calloc(tam, sizeof(int)); 
+    }
+
+   //int fd1[2];
    int status, pid;
-    pipe(fd1);                
-    
-    
-    pid = fork();   
-    if(pid == 0)         /* hijo 1*/
-    {   
-         close(fd1[READ_END]);  
-        int matriz[tam][tam];
-        matriz[0][0]= multuplicacionFilxCol(matriz1,matriz2,tam, 0,0);
-        i = 1;
-        write (fd1[WRITE_END],&matriz ,sizeof(matriz));
-        close(fd1[WRITE_END]);
-    }
-    else               /* padre */
-    {   
-        close(fd1[WRITE_END]);
-        int matriz[tam][tam];
-        read(fd1[READ_END],&matriz, sizeof(matriz));
-        close(fd1[READ_END]);
-        printf("Timpo matrixP %d segundos", i );
-     
-
+   //pipe(fd1);                
+   for(int i = 0; i<tam; i++){
+   for(int j = 0; j<tam; j++){
+          pid = fork();   
+          if(pid > 0)         /* padre */
+            {  
+            continue;
+            }
+            else               /* N hijos */
+            {   
+              //int indice;
+              ///close(fd1[READ_END]);
+              
+              matriz[i][j] = multuplicacionFilxCol(matriz1,matriz2,tam, i,j);
+              almacenarMatrix(matriz, tam);
+              //write (fd1[WRITE_END],&indice ,sizeof(int));
+              //close(fd1[WRITE_END]);
+              
+              //exit(0);
+            }  
+   }     
         
-
-        
-    }
-	 
-   /* wait para cada hijo */	
-    wait(&status);	
-   
-        matriz3 = multuplicacionMatrices(matriz1, matriz2, tam);
-        imprimirMatriz(matriz3, tam);
-        
-          
-
-   //clock_t fin= clock();
-   time_t fin =  time(NULL);
-   //tiempo += (double)(fin - inicio) / CLOCKS_PER_SEC;
-   tiempo = fin - inicio;
-   guardarTiempos(tiempo);
+   }
+   //wait(&status);
    liberarMemoria(matriz1,tam);
    liberarMemoria(matriz2,tam);
-   return tiempo;
+   return 1;
 }
 
 double matrizPciclo(int tam){
-   double tiempoMatrixP, tiempoRes;
-   for(int i = 0; i < 100; i++){
-      tiempoMatrixP +=  MatrixP(tam);
+   double  tiempoRes;
+   for(int i = 0; i < 10; i++){
+      double tiempo= 0.0;
+      time_t inicio =  time(NULL);
+      MatrixP(tam);
+       //clock_t fin= clock();
+      time_t fin =  time(NULL);
+   //tiempo += (double)(fin - inicio) / CLOCKS_PER_SEC;
+      tiempo = fin - inicio;
+      guardarTiempos(tiempo);
+       tiempoRes += tiempo;
    }
-    tiempoRes =  tiempoMatrixP /100;
+    tiempoRes =  tiempoRes /100;
+    FILE *fichero = fopen( "Stats.txt", "a" );
+    fprintf( fichero, "Promedio: ");
+    fprintf( fichero, "%f ",  tiempoRes);
+    fprintf( fichero, "\n" );
+    fclose( fichero );
     return  tiempoRes;
 
 }
@@ -192,8 +183,8 @@ double matrizPciclo(int tam){
 
  
 int main(void){
-    double tiempoMatrixP;
-    tiempoMatrixP = MatrixP(3);
+    
+   matrizPciclo(2);
     //printf("Timpo matrixP %f segundos", tiempoMatrixP );
 
    /*int **matriz1, **matriz2, **matriz, tam;
